c1d5f34d0c4c4e35c64d60e35a0a384783c9b773
LeonCromwell <mailyhai814@gmail.com>
2023-11-12 22:55:30 +0700

    fix lỗi trang register

--- src/resources/views/pages/Login/Login.php
+++ src/resources/views/pages/Login/Login.php
@@ -87,19 +87,7 @@ $connect = Connect();
             $_SESSION['current_user_email'] = '';
         }
 
-        // //connect to db
-        // $servername = "localhost";
-        // $dbname = 'php';
-        // $username = 'mailyhai';
-        // $password = '992003hai';
-        // try {
-        //     $connect = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-        //     $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        //     // echo 'Connect successfully';
-        // } catch (PDOException $e) {
-        //     echo "Connection falied: " . $e->getMessage();
-        // }
-        
+       
 
         function ValidateEmail($email)
         {
--- src/resources/views/pages/Register/Register.php
+++ src/resources/views/pages/Register/Register.php
@@ -1,3 +1,8 @@
+<?php
+include_once("../../../../../util/db/index.php");
+
+$connect = Connect();
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -108,20 +113,7 @@
             </div>
         </form>
         <?php
-        //Connect db
-        $servername = "localhost";
-        $dbname = 'php';
-        $username = 'mailyhai';
-        $password = '992003hai';
-        try {
-            $connect = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-            $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            // echo 'Connect successfully';
-        } catch (PDOException $e) {
-            echo "Connection falied: " . $e->getMessage();
-        }
-
-
+    
         //validate email
         function ValidateEmail($email)
         {
